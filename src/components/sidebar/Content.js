d54fb8d0b79c41d4682d697195aeb0d18ae90960
AbelG9 <55357845+AbelG9@users.noreply.github.com>
2020-07-24 01:36:51 -0500

    interaz lista de citas agregado
    falta mostrar datos

--- src/components/sidebar/Content.js
+++ src/components/sidebar/Content.js
@@ -5,6 +5,7 @@ import Topbar from "./Topbar";
 import PrivateRoute from "../../PrivateRoute";
 import Profile from "../contents/Profile";
 import Createcita from "../contents/Createcita";
+import Listacitas from "../contents/ListaCitas";
 
 const Content = ({ sidebarIsOpen, toggleSidebar }) => (
   <Container
@@ -13,7 +14,7 @@ const Content = ({ sidebarIsOpen, toggleSidebar }) => (
   >
     <Topbar toggleSidebar={toggleSidebar} />
       <PrivateRoute exact path="/paciente/perfil" component={Profile} />
-      <PrivateRoute exact path="/paciente/listacitas" component={() => "Lista de Citas"} />
+      <PrivateRoute exact path="/paciente/listacitas" component={Listacitas} />
       <PrivateRoute exact path="/paciente/obtenercita" component={Createcita} />
       <PrivateRoute exact path="/paciente/triaje" component={() => "Triaje"} />
   </Container>
